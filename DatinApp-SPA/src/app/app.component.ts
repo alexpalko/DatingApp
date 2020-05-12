c200721754bd043ad06c0e615c62710b859bfc06
Alex Palko <43597659+alexpalko@users.noreply.github.com>
2020-05-12 20:53:32 +0300

    Added third party components

--- DatinApp-SPA/src/app/app.component.ts
+++ DatinApp-SPA/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Dating app';
+export class AppComponent implements OnInit {
+  jwtHelper = new JwtHelperService();
+
+  constructor(private authService: AuthService) {}
+
+  // Every the app loads, authService will have the decodedToken assigned, if the user is already logged in.
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decodedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
--- DatinApp-SPA/src/app/app.module.ts
+++ DatinApp-SPA/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +23,9 @@ import { ErrorInterceptorProvider } from './_services/error.incerceptor';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot(),
    ],
    providers: [
       ErrorInterceptorProvider
